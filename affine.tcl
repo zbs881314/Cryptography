e6f6d52fe976342e55c38a0caeba5aa9d1ea287a
zbs881314 <34663445+zbs881314@users.noreply.github.com>
2019-01-03 15:42:47 -0500

    Create affine.tcl

---
+++ affine.tcl
@@ -0,0 +1,15 @@
+#!/usr/bin/tclsh
+
+set plain [split ZABCDEFGHIJKLMNOPQRSTUVWXY ""]
+
+lappend argv 1
+lassign $argv shift mult
+
+for {set P 0} {$P<26} {incr P} {
+set C [expr {($P*$mult+$shift)%26}]
+set arr([lindex $plain $P]) [lindex $plain $C]
+}
+
+set data [string toupper [read stdin]] 
+
+puts [string map [array get arr] $data]
